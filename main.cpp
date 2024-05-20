8d4a2153cf4a0894d10e4aec562b9f6e7a15be91
m-sadkowski <msadkowski000@gmail.com>
2024-05-21 00:59:22 +0200

    aktualizacja 21.05
    + 20%

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 class MyVector {
-    int size;
     int* tab;
     int capacity;
 
 public:
+    int size;
     MyVector() {
         size = 0;
         tab = new int[1];
         capacity = 1;
     }
 
-    MyVector(int size) {
+    explicit MyVector(int size) {
         this->size = size;
         tab = new int[size];
         capacity = size;
@@ -58,13 +58,14 @@ public:
         }
     }
 
-    int get(int index) {
+    int get(int index) const {
         if (index >= 0 && index < size) {
             return tab[index];
         }
+        return 0;
     }
 
-    int getSize() {
+    int getSize() const {
         return size;
     }
 
@@ -87,6 +88,7 @@ public:
         if (index >= 0 && index < size) {
             return tab[index];
         }
+        return tab[0];
     }
 };
 
@@ -119,81 +121,100 @@ void quickSort(int* tab, int size) {
 
 //ci¹gu stopniowy 
 // (ci¹g n liczb)
-void degreeSequence (MyVector* graph, int numberOfNodes) {
+void degreeSequence(MyVector* graph, int numberOfNodes) {
     int* degree = new int[numberOfNodes];
     for (int i = 0; i < numberOfNodes; i++) {
-        if (graph[i][0] == -1) {
-			degree[i] = 0;
-        }
-        else {
-			degree[i] = graph[i].getSize();
-		}
+        degree[i] = graph[i].getSize();
     }
     quickSort(degree, numberOfNodes);
-    for (int i = 0; i < numberOfNodes; i++) {
-		printf("%d ", degree[i]);
-	}
+    for (int i = numberOfNodes - 1; i >= 0; i--) {
+        printf("%d ", degree[i]);
+    }
+    delete[] degree;
     return;
 }
 
-/*
-//liczba sk³adowych spójnoœci 
-// (true/false)
-void numberOfComponents (MyVector* graph, int numberOfNodes) {
-
+//liczba sk³adowych spójnoœci
+// (liczba)
+bool DFS(MyVector* graph, int node, int* visited, int numberOfNodes) {
+    visited[node] = 1;
+    for (int i = 0; i < graph[node].getSize(); i++) {
+        if (visited[graph[node].get(i) - 1] == 0) {
+            DFS(graph, graph[node].get(i) - 1, visited, numberOfNodes);
+        }
+    }
+    return true;
 }
 
-//dwudzielnoœæ grafu 
+void numberOfComponents(MyVector* graph, int numberOfNodes) {
+    int* visited = new int[numberOfNodes];
+    for (int i = 0; i < numberOfNodes; i++) {
+        visited[i] = 0;
+    }
+    int components = 0;
+    for (int i = 0; i < numberOfNodes; i++) {
+        if (visited[i] == 0) {
+            DFS(graph, i, visited, numberOfNodes);
+            components++;
+        }
+    }
+    printf("%d", components);
+    delete[] visited;
+    return;
+}
+
+
+//dwudzielnoœæ grafu
 // (true/false)
 void bipartiteness(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
 
-//acentrycznoœci  wierzcho³ków (w ramach sk³adowych spójnoœci) 
+//acentrycznoœci  wierzcho³ków (w ramach sk³adowych spójnoœci)
 // (ci¹g n liczb)
 void eccentricityOfVertices(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
 
 //planarnoœæ grafu (true/false)
 void planarity(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
 
 // kolorowanie wierzcho³ków grafu algorytmem zach³annym
-// w kolejnoœci wierzcho³ków zgodnej z numerem wierzcho³ka 
+// w kolejnoœci wierzcho³ków zgodnej z numerem wierzcho³ka
 // (ci¹g n liczb)
 void verticesColoursGreedy(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
 
 // kolorowanie wierzcho³ków grafu algorytmem LF
-// (remisy rozwi¹zujemy przy pomocy numeru wierzcho³ka) 
+// (remisy rozwi¹zujemy przy pomocy numeru wierzcho³ka)
 // (ci¹g n liczb)
 void verticesColoursLF(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
 
-// kolorowanie wierzcho³ków grafu algorytmem SLF  
-// (w przypadku remisu wybierz wierzcho³ek z najwy¿szym stopniem, 
-// a jeœli nadal jest remis to wierzcho³ek o najmniejszym indeksie) 
+// kolorowanie wierzcho³ków grafu algorytmem SLF
+// (w przypadku remisu wybierz wierzcho³ek z najwy¿szym stopniem,
+// a jeœli nadal jest remis to wierzcho³ek o najmniejszym indeksie)
 // (ci¹g n liczb)
 void verticesColoursSLF(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
 
-// liczba ró¿nych podgrafów C4 
+// liczba ró¿nych podgrafów C4
 // (liczba)
 void c4Subgraphs(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
 
-// liczba krawêdzi dope³nienia grafu 
+// liczba krawêdzi dope³nienia grafu
 // (liczba)
 void complementsEdges(MyVector* graph, int numberOfNodes) {
-
+    printf("\n?");
 }
-*/
+
 
 void inputGraph() {
     int numberOfNodes = 0;
@@ -207,9 +228,6 @@ void inputGraph() {
     for (int i = 0; i < numberOfNodes; i++) {
         scanf("%d", &numberOfNeighbours);
         MyVector neighbours;
-        if (numberOfNeighbours == 0) {
-            neighbours.push_back(-1);
-        }
         for (int j = 0; j < numberOfNeighbours; j++) {
             scanf("%d", &neighbour);
             neighbours.push_back(neighbour);
@@ -221,34 +239,45 @@ void inputGraph() {
     printf("\n");
     for (int i = 0; i < numberOfNodes; i++) {
         for (int j = 0; j < graph[i].getSize(); j++) {
-			printf("%d ", graph[i].get(j));
-		}
-		printf("\n");
-	}
+            printf("%d ", graph[i].get(j));
+        }
+        printf("\n");
+    }
     */
 
-    printf("\n1. Ciag stopniowy: ");
+    printf("\n");
     degreeSequence(graph, numberOfNodes);
-    /*
-    printf("\n2. Liczba skladowych spojnosci: ");
+
+    printf("\n");
     numberOfComponents(graph, numberOfNodes);
-    printf("\n3. Dwudzielnosc grafu: ");
+
+    printf("\n");
     bipartiteness(graph, numberOfNodes);
-    printf("\n4. Acentrycznosc wierzcholkow: ");
+
+    printf("\n");
     eccentricityOfVertices(graph, numberOfNodes);
-    printf("\n5. Planarnosc grafu: ");
+
+    printf("\n");
     planarity(graph, numberOfNodes);
-    printf("\n6. Kolory wierzcholkow (zachlannie): ");
+
+    printf("\n");
     verticesColoursGreedy(graph, numberOfNodes);
-    printf("\n7. Kolory wierzcholkow (LF): ");
+
+    printf("\n");
     verticesColoursLF(graph, numberOfNodes);
-    printf("\n8. Kolory wierzcholkow (SLF): ");
+
+    printf("\n");
     verticesColoursSLF(graph, numberOfNodes);
-    printf("\n9. Liczba roznych podgrafow C4: ");
+
+    printf("\n");
     c4Subgraphs(graph, numberOfNodes);
-    printf("\n10. Liczba krawedzi dopelnienia grafu: ");
-    complementsEdges(graph, numberOfNodes); */
+
+    printf("\n");
+    complementsEdges(graph, numberOfNodes);
+
     printf("\n\n");
+
+    delete[] graph;
 }
 
 int main() {
